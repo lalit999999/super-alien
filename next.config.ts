9e4e8d7aea2935d7c909b2fff0b83452ac557306
Lalit gurjar <gujarlalit79@gmail.com>
2026-06-18 11:43:23 +0530

    resolving proxy issue

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: [
+    "https://affix-deniable-crayfish.ngrok-free.dev/",
+    "*.ngrok-free.dev",
+    "*.ngrok-free.app",
+  ],
 };
 
 export default nextConfig;
